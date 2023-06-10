adb1633cebc3d24af5d656ea0652b69ef15eca85
freddyrondon <123912473+freddyrondon@users.noreply.github.com>
2023-06-10 01:06:18 +0000

    Adding LittleLemonQueries to the repository

--- LittleLemonQueries.sql
+++ LittleLemonQueries.sql
@@ -1,50 +1,50 @@
--- View for orders with quantity greater than 2
-CREATE VIEW OrdersView AS (
-SELECT OrderID, Quantity, Bill_Amount
-FROM Orders
-WHERE Quantity > 2
-);
--- Order details for orders with cost greater than 150
-SELECT
-	c.CustomerID,
-    c.FullName,
-    o.OrderID,
-    o.TotalCost,
-    m.Cuisine,
-    mi.CourseName
-FROM Orders o
-INNER JOIN Customers c
-  ON o.CustomerID = c.CustomerID
-INNER JOIN Menus m
-  ON o.MenuID = m.MenuID
-INNER JOIN MenuItems mi
-  ON m.MenuItemsID = mi.MenuItemsID
-WHERE o.TotalCost > 150
-ORDER BY o.TotalCost;
--- stored procedure to get the order with the max quantity 
-CREATE PROCEDURE GetMaxQuantity()  
-SELECT MAX(Quantity) AS "Max Quantity in Order"
-FROM Orders;
+### "Creating the procedure GetMaxQuantity"
+
+USE LittleLemonDB;
+DROP PROCEDURE IF EXISTS GetMaxQuantity;
+CREATE PROCEDURE GetMaxQuantity() SELECT MAX(Quantity) AS GetMaxQuantity FROM Orders;
 CALL GetMaxQuantity();
--- prepared statement to get order details using user input order id
-PREPARE GetOrderDetail 
-FROM 'SELECT OrderID, Quantity, TotalCost FROM Orders WHERE OrderID = ?';
-SET @id = 1;
-EXECUTE GetOrderDetail USING @id;
--- stored procedure to cancel a booking
-DROP procedure CancelBooking;
-DELIMITER //
-CREATE PROCEDURE CancelBooking(IN CancelID INT)
+
+### "Creating the procedure ManageBooking" 
+
+USE LittleLemonDB;
+DROP PROCEDURE IF EXISTS ManageBooking;
+CREATE PROCEDURE ManageBooking(Date DATE,TableNumber INT)
+	SELECT CONCAT("Table ", TableNumber, " is already booked")
+    WHERE EXISTS (SELECT * FROM Bookings WHERE Date = Date and TableNumber = TableNumber)
+CALL ManageBooking("2022-23-01", 3);
+
+### "Creating the procedure UpdateBooking"
+
+DROP PROCEDURE IF EXISTS UpdateBooking;
+DELIMITER $$ 
+CREATE PROCEDURE UpdateBooking(BookingID INT, Date DATE) 
 BEGIN
-DELETE FROM Bookings WHERE BookingID = CancelID;
-SELECT CONCAT("Order ",CancelID, " is cancelled.") AS Confirmation FROM Bookings;
-END//
-DELIMITER ;
-CALL CancelBooking(5);
--- update booking procedure
-CREATE PROCEDURE UpdateBooking(IN UpdateID INT, IN TableNo INT)
-UPDATE Bookings
-SET TableNumber = TableNo
-WHERE BookingID = UpdateID;
-CALL UpdateBooking(6, 1);
-SELECT * FROM Bookings;
+UPDATE Bookings SET DATE = Date WHERE BookingID = BookingID; 
+SELECT CONCAT("Booking ", BookingID, " updated") AS "Confirmation"; 
+END$$ 
+DELIMITER ; 
+CALL UpdateBooking(4, "2022-27-01");
+
+### "Creating the procedure AddBooking"
+
+DROP PROCEDURE IF EXISTS AddBooking; 
+DELIMITER $$ 
+CREATE PROCEDURE AddBooking(BookingID INT, CustomerID INT, TableNumber INT, BookingDate DATE)
+BEGIN 
+INSERT INTO Bookings (booking_id, customer_id, table_number, date) VALUES (BookingID, CustomerID, TableNumber, Date); 
+SELECT "New booking added" AS "Confirmation";
+END$$ DELIMITER ; 
+CALL AddBooking(9, 3, 4, "2022-28-01");
+
+
+### "Creating the procedure CancelBooking"
+
+DROP PROCEDURE IF EXISTS CancelBooking; 
+DELIMITER $$ 
+CREATE PROCEDURE CancelBooking(BookingID INT) 
+BEGIN 
+DELETE FROM Bookings WHERE booking_id = BookingID; SELECT CONCAT("Booking ", BookingID, " cancelled") AS "Confirmation"; 
+END$$ 
+DELIMITER ; 
+CALL CancelBooking(2);
